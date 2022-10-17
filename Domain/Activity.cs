3ebc228a3ae33755960920951f897b7255b17e2f
giviscream <giviscream@gmail.com>
2022-10-17 20:50:40 +0300

    Add Many to many user activity

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -10,5 +10,7 @@ namespace Domain
         public string City { get; set; }
         public string Venue { get; set; }
 
+        public ICollection<ActivityAttendee> Attendees { get; set; }
+
     }
 }
